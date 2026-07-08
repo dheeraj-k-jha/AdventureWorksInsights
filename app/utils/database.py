08511c50cfa27c2200fb0581ab9376359ecdc817
Dheeraj jha <justdheeit101@gmail.com>
2026-07-08 15:25:56 +0530

    fixed database fetch issue

--- app/utils/database.py
+++ app/utils/database.py
@@ -2,8 +2,11 @@ import pandas as pd
 from sqlalchemy import create_engine
 import config
 
+# Credentials:
 engine = create_engine(
-    f"postgresql+psycopg2://{config.DB_USER}:{config.DB_PASSWORD}@localhost:5432/adventureworks"
+    f"postgresql+psycopg2://{config.DB_USER}:{config.DB_PASSWORD}"
+    f"@{config.DB_HOST}:{config.DB_PORT}/{config.DB_NAME}",
+    connect_args={"sslmode": "require"}
 )
 
 def run_query(query):
